5b7739c1f19806f2ee840a1a475c3ae317636bbf
AG046737 <ag046737@cerner.com>
2018-05-11 02:49:33 -0500

    [#21] more use cases for parsing product version from metadata.yml

--- tasks/stage-product/task.sh
+++ tasks/stage-product/task.sh
@@ -13,10 +13,20 @@ PRODUCT_NAME="$(
 		cut -d' ' -f 2
 )"
 
+# I know, this is kind of bonkers. But here's the deal:
+# We have to account for all these possible values (and maybe more?):
+# product_version: '1.12.9-beta.11'
+# product_version: 1.12.9-beta.11
+# product_version: "1.12.9-beta.11"
+# product_version: '1.12.9'
+# product_version: 1.12.9
+# product_version: "1.12.9"
+# ... So that's what the weird tr -d is for
 DESIRED_VERSION=$(
 	cat metadata/*.yml |
 		grep '^product_version' |
-		cut -d' ' -f 2
+		cut -d' ' -f 2 |
+		tr -d "'\""
 )
 
 om-linux \
