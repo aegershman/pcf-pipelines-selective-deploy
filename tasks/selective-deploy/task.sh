19caa13e27817eaa499c75bbd234c469cc0a0d32
AG046737 <ag046737@cerner.com>
2018-05-11 01:48:52 -0500

    [#1] adding apply-changes in selective deploy to try and re-attach to log stream

--- tasks/selective-deploy/task.sh
+++ tasks/selective-deploy/task.sh
@@ -35,3 +35,20 @@ om-linux \
 	--path /api/v0/installations \
 	--request POST \
 	--data "$DATA"
+
+# This *should* reattach the log stream to the current apply-changes job
+# We shouldn't have to parse the "installation_id" returned from the POST
+# above; we should be able to just do "apply-changes" and instead of it
+# actually applying changes, it reattaches to the log stream... Hopefully?
+#
+# Also, just in case this does screw up somehow, we're passing in
+# skip-deploy-products in the hopes that it only applies changes
+# to the director & not the entire foundation. That shouldn't happen,
+# but just in case...
+om-linux \
+	--target "https://${OPSMAN_DOMAIN_OR_IP_ADDRESS}" \
+	--client-id "${OPSMAN_CLIENT_ID}" \
+	--client-secret "${OPSMAN_CLIENT_SECRET}" \
+	--skip-ssl-validation \
+	apply-changes \
+	--skip-deploy-products="true"
